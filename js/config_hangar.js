75cfe89f54f609281720f7fff30a8caccffa6394
giuliaognibene1 <giuliaognibene@gmail.com>
2026-04-23 17:46:17 +0200

    Add files via upload

---
+++ js/config_hangar.js
@@ -0,0 +1,571 @@
+const dati = {
+
+    // MODELLO
+    mod_10_40: {
+    "MODELLO 10,40": 1
+    },
+    mod_13_30: {
+    "MODELLO 13,30": 1
+    },
+
+    // LUNGHEZZE
+    lung_12_00: {
+        comuni: {
+        "TRAVE 6,60m": 2,
+        "TRAVE 5,40m": 2,
+        "TUBO D.48 L.6m": 19,
+        "CAVALLOTTO A TETTO x TUBO D.48": 54,
+        "BAIONETTA x TUBO D.48": 18,
+        "KIT BULLONI x TUBO D.48": 1,
+        "LAMIERA CALANDRATA 6,60m": 34,
+        "LAMIERA CALANDRATA 3,00m": 17,
+        "FARFALLE x LAMIERA": 170,
+        "KIT BULLONI x LAMIERA": 1,
+        "GRONDAIETTA 2,50m": 2,
+        "GRONDAIETTA 3,30m": 6,
+        "TASSELLO x GRONDAIETTA": 24
+        },
+        mod_10_40: {
+        "TUBO ARCATO D.101": 12,
+        "KIT BULLONI x TUBO D.101": 1
+        },
+        mod_13_30: {
+        "TUBO ARCATO FLANGIA TONDA D.140": 12,
+        "TUBO ARCATO FLANGIA QUADRA D.140": 12,
+        "KIT BULLONI x TUBO D.101": 1,
+        }
+    },
+    lung_14_20: {
+        comuni: {
+        "TRAVE 6,60m": 2,
+        "TRAVE 7,60m": 2,
+        "TUBO D.48 L.6m": 23,
+        "CAVALLOTTO A TETTO x TUBO D.48": 63,
+        "BAIONETTA x TUBO D.48": 18,
+        "KIT BULLONI x TUBO D.48": 1,
+        "LAMIERA CALANDRATA 6,60m": 40,
+        "LAMIERA CALANDRATA 3,00m": 20,
+        "FARFALLE x LAMIERA": 50,
+        "KIT BULLONI x LAMIERA": 1,
+        "GRONDAIETTA 1,40m": 2,
+        "GRONDAIETTA 3,30m": 8,
+        "TASSELLO x GRONDAIETTA": 1
+        },
+        mod_10_40: {
+        "TUBO ARCATO D.101": 14,
+        "KIT BULLONI x TUBO D.101": 1
+        },
+        mod_13_30: {
+        "TUBO ARCATO FLANGIA TONDA D.140": 7,
+        "TUBO ARCATO FLANGIA QUADRA D.140": 14,
+        "KIT BULLONI x TUBO D.101": 1,
+        }
+    },
+    lung_18_60: {
+        comuni: {
+        "TRAVE 6,60m": 4,
+        "TRAVE 5,40m": 2,
+        "TUBO D.48 L.6m": 32,
+        "CAVALLOTTO A TETTO x TUBO D.48": 81,
+        "BAIONETTA x TUBO D.48": 27,
+        "KIT BULLONI x TUBO D.48": 1,
+        "LAMIERA CALANDRATA 6,60m": 52,
+        "LAMIERA CALANDRATA 3,00m": 26,
+        "FARFALLE x LAMIERA": 170,
+        "KIT BULLONI x LAMIERA": 1,
+        "GRONDAIETTA 2,50m": 2,
+        "GRONDAIETTA 3,30m": 10,
+        "TASSELLO x GRONDAIETTA": 24
+        },
+        mod_10_40: {
+        "TUBO ARCATO D.101": 18,
+        "KIT BULLONI x TUBO D.101": 1
+        },
+        mod_13_30: {
+        "TUBO ARCATO FLANGIA TONDA D.140": 9,
+        "TUBO ARCATO FLANGIA QUADRA D.140": 18,
+        "KIT BULLONI x TUBO D.101": 1,
+        }
+    },
+    lung_20_80: {
+        comuni: {
+        "TRAVE 6,60m": 4,
+        "TRAVE 7,60m": 2,
+        "TUBO D.48 L.6m": 32,
+        "CAVALLOTTO A TETTO x TUBO D.48": 90,
+        "BAIONETTA x TUBO D.48": 27,
+        "KIT BULLONI x TUBO D.48": 1,
+        "LAMIERA CALANDRATA 6,60m": 58,
+        "LAMIERA CALANDRATA 3,00m": 29,
+        "FARFALLE x LAMIERA": 50,
+        "KIT BULLONI x LAMIERA": 1,
+        "GRONDAIETTA 1,40m": 2,
+        "GRONDAIETTA 3,30m": 12,
+        "TASSELLO x GRONDAIETTA": 1
+        },
+        mod_10_40: {
+        "TUBO ARCATO D.101": 20,
+        "KIT BULLONI x TUBO D.101": 1
+        },
+        mod_13_30: {
+        "TUBO ARCATO FLANGIA TONDA D.140": 10,
+        "TUBO ARCATO FLANGIA QUADRA D.140": 20,
+        "KIT BULLONI x TUBO D.101": 1,
+        }
+    },
+    lung_25_20: {
+        comuni: {
+        "TRAVE 6,60m": 6,
+        "TRAVE 5,40m": 2,
+        "TUBO D.48 L.6m": 41,
+        "CAVALLOTTO A TETTO x TUBO D.48": 108,
+        "BAIONETTA x TUBO D.48": 36,
+        "KIT BULLONI x TUBO D.48": 1,
+        "LAMIERA CALANDRATA 6,60m": 70,
+        "LAMIERA CALANDRATA 3,00m": 35,
+        "FARFALLE x LAMIERA": 170,
+        "KIT BULLONI x LAMIERA": 1,
+        "GRONDAIETTA 2,50m": 2,
+        "GRONDAIETTA 3,30m": 14,
+        "TASSELLO x GRONDAIETTA": 24
+        },
+        mod_10_40: {
+        "TUBO ARCATO D.101": 24,
+        "KIT BULLONI x TUBO D.101": 1
+        },
+        mod_13_30: {
+        "TUBO ARCATO FLANGIA TONDA D.140": 12,
+        "TUBO ARCATO FLANGIA QUADRA D.140": 24,
+        "KIT BULLONI x TUBO D.101": 1,
+        }
+    },
+    lung_27_40: {
+        comuni: {
+        "TRAVE 6,60m": 6,
+        "TRAVE 7,60m": 2,
+        "TUBO D.48 L.6m": 45,
+        "CAVALLOTTO A TETTO x TUBO D.48": 117,
+        "BAIONETTA x TUBO D.48": 36,
+        "KIT BULLONI x TUBO D.48": 1,
+        "LAMIERA CALANDRATA 6,60m": 76,
+        "LAMIERA CALANDRATA 3,00m": 38,
+        "FARFALLE x LAMIERA": 50,
+        "KIT BULLONI x LAMIERA": 1,
+        "GRONDAIETTA 1,40m": 2,
+        "GRONDAIETTA 3,30m": 16,
+        "TASSELLO x GRONDAIETTA": 1
+        },
+        mod_10_40: {
+        "TUBO ARCATO D.101": 26,
+        "KIT BULLONI x TUBO D.101": 1
+        },
+        mod_13_30: {
+        "TUBO ARCATO FLANGIA TONDA D.140": 13,
+        "TUBO ARCATO FLANGIA QUADRA D.140": 26,
+        "KIT BULLONI x TUBO D.101": 1,
+        }
+    },
+    lung_31_80: {
+        comuni: {
+        "TRAVE 6,60m": 8,
+        "TRAVE 5,40m": 2,
+        "TUBO D.48 L.6m": 50,
+        "CAVALLOTTO A TETTO x TUBO D.48": 135,
+        "BAIONETTA x TUBO D.48": 45,
+        "KIT BULLONI x TUBO D.48": 1,
+        "LAMIERA CALANDRATA 6,60m": 88,
+        "LAMIERA CALANDRATA 3,00m": 44,
+        "FARFALLE x LAMIERA": 170,
+        "KIT BULLONI x LAMIERA": 1,
+        "GRONDAIETTA 2,50m": 2,
+        "GRONDAIETTA 3,30m": 18,
+        "TASSELLO x GRONDAIETTA": 24
+        },
+        mod_10_40: {
+        "TUBO ARCATO D.101": 30,
+        "KIT BULLONI x TUBO D.101": 1
+        },
+        mod_13_30: {
+        "TUBO ARCATO FLANGIA TONDA D.140": 15,
+        "TUBO ARCATO FLANGIA QUADRA D.140": 30,
+        "KIT BULLONI x TUBO D.101": 1,
+        }
+    },
+    lung_34_00: {
+        comuni: {
+        "TRAVE 6,60m": 8,
+        "TRAVE 7,60m": 2,
+        "TUBO D.48 L.6m": 54,
+        "CAVALLOTTO A TETTO x TUBO D.48": 144,
+        "BAIONETTA x TUBO D.48": 45,
+        "KIT BULLONI x TUBO D.48": 1,
+        "LAMIERA CALANDRATA 6,60m": 94,
+        "LAMIERA CALANDRATA 3,00m": 47,
+        "FARFALLE x LAMIERA": 59,
+        "KIT BULLONI x LAMIERA": 1,
+        "GRONDAIETTA 1,40m": 2,
+        "GRONDAIETTA 3,30m": 20,
+        "TASSELLO x GRONDAIETTA": 1
+        },
+        mod_10_40: {
+        "TUBO ARCATO D.101": 32,
+        "KIT BULLONI x TUBO D.101": 1
+        },
+        mod_13_30: {
+        "TUBO ARCATO FLANGIA TONDA D.140": 16,
+        "TUBO ARCATO FLANGIA QUADRA D.140": 32,
+        "KIT BULLONI x TUBO D.101": 1,
+        }
+    },
+    lung_38_40: {
+        comuni: {
+        "TRAVE 6,60m": 10,
+        "TRAVE 5,40m": 2,
+        "TUBO D.48 L.6m": 59,
+        "CAVALLOTTO A TETTO x TUBO D.48": 162,
+        "BAIONETTA x TUBO D.48": 54,
+        "KIT BULLONI x TUBO D.48": 1,
+        "LAMIERA CALANDRATA 6,60m": 106,
+        "LAMIERA CALANDRATA 3,00m": 53,
+        "FARFALLE x LAMIERA": 170,
+        "KIT BULLONI x LAMIERA": 1,
+        "GRONDAIETTA 2,50m": 2,
+        "GRONDAIETTA 3,30m": 22,
+        "TASSELLO x GRONDAIETTA": 24
+        },
+        mod_10_40: {
+        "TUBO ARCATO D.101": 36,
+        "KIT BULLONI x TUBO D.101": 1
+        },
+        mod_13_30: {
+        "TUBO ARCATO FLANGIA TONDA D.140": 18,
+        "TUBO ARCATO FLANGIA QUADRA D.140": 36,
+        "KIT BULLONI x TUBO D.101": 1,
+        }
+    },
+    lung_40_60: {
+        comuni: {
+        "TRAVE 6,60m": 10,
+        "TRAVE 7,60m": 2,
+        "TUBO D.48 L.6m": 63,
+        "CAVALLOTTO A TETTO x TUBO D.48": 171,
+        "BAIONETTA x TUBO D.48": 54,
+        "KIT BULLONI x TUBO D.48": 1,
+        "LAMIERA CALANDRATA 6,60m": 112,
+        "LAMIERA CALANDRATA 3,00m": 56,
+        "FARFALLE x LAMIERA": 50,
+        "KIT BULLONI x LAMIERA": 1,
+        "GRONDAIETTA 1,40m": 2,
+        "GRONDAIETTA 3,30m": 24,
+        "TASSELLO x GRONDAIETTA": 1
+        },
+        mod_10_40: {
+        "TUBO ARCATO D.101": 38,
+        "KIT BULLONI x TUBO D.101": 1
+        },
+        mod_13_30: {
+        "TUBO ARCATO FLANGIA TONDA D.140": 19,
+        "TUBO ARCATO FLANGIA QUADRA D.140": 38,
+        "KIT BULLONI x TUBO D.101": 1,
+        }
+    },
+    lung_45_00: {
+        comuni: {
+        "TRAVE 6,60m": 12,
+        "TRAVE 5,40m": 2,
+        "TUBO D.48 L.6m": 72,
+        "CAVALLOTTO A TETTO x TUBO D.48": 198,
+        "BAIONETTA x TUBO D.48": 63,
+        "KIT BULLONI x TUBO D.48": 1,
+        "LAMIERA CALANDRATA 6,60m": 124,
+        "LAMIERA CALANDRATA 3,00m": 62,
+        "FARFALLE x LAMIERA": 170,
+        "KIT BULLONI x LAMIERA": 1,
+        "GRONDAIETTA 2,50m": 2,
+        "GRONDAIETTA 3,30m": 26,
+        "TASSELLO x GRONDAIETTA": 24
+        },
+        mod_10_40: {
+        "TUBO ARCATO D.101": 12,
+        "KIT BULLONI x TUBO D.101": 1
+        },
+        mod_13_30: {
+        "TUBO ARCATO FLANGIA TONDA D.140": 12,
+        "TUBO ARCATO FLANGIA QUADRA D.140": 12,
+        "KIT BULLONI x TUBO D.101": 1,
+        }
+    },
+
+    // TESTATA CON PORTONE
+    una_testata_portone: {
+    "ANTA DESTRA PORTONE": 1,
+    "ANTA SINISTRA PORTONE": 1,
+    "GUIDA INFERIORE": 1,
+    "GUIDA SUPERIORE": 1
+    },
+    due_testata_portone: {
+    "ANTA DESTRA PORTONE": 2,
+    "ANTA SINISTRA PORTONE": 2,
+    "GUIDA INFERIORE": 1,
+    "GUIDA SUPERIORE": 1
+    },
+
+    // TESTATA CHIUSA
+    zero_testata_chiusa: { },
+    una_testata_chiusa: {
+    "12-8187997 - KIT 2° TIMER BARRIERA + AUTOREVERSE E iCR": 1,
+    "12-EOF-PKGCP600-HP - KIT PACCO HARDWARE AUTOREV. BARRIERA HP": 1,
+    "12-EOFBLHP-CP600 - KIT TRUSS PACCO ANGOLARI BARRIERA HP": 1,
+    "12-98.200390-11 - BARRIERA FINE CAMPO ASSEMBLATA": 2
+    }
+};
+
+/* =====================================================
+   CALCOLO CONFIGURAZIONE
+   ===================================================== */
+function nomePulito(valore) {
+
+    const nomi = {
+        mod_10_40: "modello 10,40",
+        mod_13_30: "modello 13,30",
+
+        lung_12_00: "12,00m",
+        lung_14_20: "14,20m",
+        lung_18_60: "18,60m",
+        lung_20_80: "20,80m",
+        lung_25_20: "25,20m",
+        lung_27_40: "27,40m",
+        lung_31_80: "31,80m",
+        lung_34_00: "34,00m",
+        lung_38_40: "38,40m",
+        lung_40_60: "40,60m",
+        lung_45_00: "45,00m",
+
+        una_testata_portone: "Una testata con portone",
+        una_testata_chiusa: "Una testata chiusa",
+        due_testata_portone: "Due testate con portone",
+        zero_testata_chiusa: "Nessuna testata chiusa",
+
+    };
+
+    return nomi[valore] || valore;
+}
+
+function aggiorna() {
+
+    let modello = document.getElementById("modello").value;
+    let totale = {};
+    let perCategoria = {};
+    let composizione = {};
+
+    function aggiungi(componenti, categoria) {
+
+        if (!perCategoria[categoria]) {
+            perCategoria[categoria] = {};
+        }
+
+        for (let pezzo in componenti) {
+
+            if (!totale[pezzo]) totale[pezzo] = 0;
+            totale[pezzo] += componenti[pezzo];
+
+            if (!perCategoria[categoria][pezzo]) {
+                perCategoria[categoria][pezzo] = 0;
+            }
+
+            perCategoria[categoria][pezzo] += componenti[pezzo];
+        }
+    }
+
+
+    /* SELECT NORMALI */
+    document.querySelectorAll("select[data-cat]").forEach(select => {
+
+        let val = select.value;
+        let cat = select.dataset.cat;
+
+        if (dati[val]) {
+
+            if (val.startsWith("lung_")) {
+                let modello = document.querySelector(
+                    'select[data-cat="Modello"]'
+                ).value;
+
+                aggiungi(dati[val].comuni, cat);
+
+                if (dati[val][modello]) {
+                    aggiungi(dati[val][modello], cat);
+                }
+
+            } else {
+                aggiungi(dati[val], cat);
+            }
+
+            if (!composizione[cat]) composizione[cat] = [];
+            composizione[cat].push(nomePulito(val));
+        }
+    });
+
+
+    /* STAMPA PER CATEGORIA */
+    let divCat = document.getElementById("perCategoria");
+    divCat.innerHTML = "";
+
+    for (let cat in perCategoria) {
+
+        let blocco = document.createElement("div");
+        blocco.className = "categoria";
+
+        let titolo = document.createElement("strong");
+        let testo = cat;
+
+        if (composizione[cat] && composizione[cat].length > 0) {
+            testo += " → " + composizione[cat].join(" → ");
+        }
+
+        titolo.textContent = testo;
+
+        blocco.appendChild(titolo);
+
+        let ul = document.createElement("ul");
+
+        for (let pezzo in perCategoria[cat]) {
+
+            let li = document.createElement("li");
+
+            li.innerHTML = `
+                <span class="qty">${perCategoria[cat][pezzo]}</span>
+                <span class="sep">x</span>
+                <span class="name">${pezzo}</span>
+            `;
+
+            ul.appendChild(li);
+        }
+
+        blocco.appendChild(ul);
+        divCat.appendChild(blocco);
+    }
+
+
+    /* STAMPA TOTALE */
+    let ulTot = document.getElementById("totale");
+    ulTot.innerHTML = "";
+
+    for (let pezzo in totale) {
+
+        let li = document.createElement("li");
+
+        li.innerHTML = `
+            <span class="qty">${totale[pezzo]}</span>
+            <span class="sep">x</span>
+            <span class="name">${pezzo}</span>
+        `;
+
+        ulTot.appendChild(li);
+    }
+}
+
+
+/* =====================================================
+   PDF
+   ===================================================== */
+
+function getBase64Image(img) {
+
+    let canvas = document.createElement("canvas");
+
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    return canvas.toDataURL("image/png");
+}
+
+
+function disegnaHeader(doc, logo, cliente, data, pagina) {
+
+    doc.addImage(logo, "PNG", 20, 10, 22, 22);
+
+    doc.setFont("helvetica", "bold");
+    doc.setFontSize(12);
+    doc.text("AGRO+ SRL", 50, 15);
+
+    doc.setFont("helvetica", "normal");
+    doc.setFontSize(9);
+    doc.text("Via Sacco e Vanzetti 21", 50, 21);
+    doc.text("42021 Bibbiano (RE)", 50, 26);
+    doc.text("info@agropiu.com - 0522 875924", 50, 31);
+
+    doc.setFontSize(10);
+    doc.text("Cliente: " + cliente, 20, 45);
+    doc.text("Data: " + data, 120, 45);
+    doc.text("Pagina " + pagina, 180, 45);
+}
+
+
+function generaPDF() {
+
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF();
+
+    let cliente = document.getElementById("cliente").value.trim();
+
+    if (!cliente) {
+        alert("Inserisci il nome del cliente prima di esportare il PDF");
+        return;
+    }
+
+    let data = new Date().toLocaleDateString();
+
+    let img = new Image();
+    img.src = "logo.png";
+
+    img.onload = function () {
+
+        let logo = getBase64Image(img);
+
+        creaPDF(doc, logo, cliente, data);
+    };
+}
+
+
+function creaPDF(doc, logo, cliente, data) {
+
+    let righe = document.querySelectorAll("#totale li");
+
+    let pageHeight = doc.internal.pageSize.height;
+
+    let yStart = 65;
+    let y = yStart;
+
+    let pagina = 1;
+
+    disegnaHeader(doc, logo, cliente, data, pagina);
+
+    righe.forEach(riga => {
+
+        let qty = riga.querySelector(".qty").innerText;
+        let sep = riga.querySelector(".sep").innerText;
+        let nome = riga.querySelector(".name").innerText;
+
+        if (y > pageHeight - 20) {
+
+            doc.addPage();
+
+            pagina++;
+            y = yStart;
+
+            disegnaHeader(doc, logo, cliente, data, pagina);
+        }
+
+        doc.text(qty, 20, y);
+        doc.text(sep, 35, y);
+        doc.text(nome, 45, y);
+
+        y += 8;
+    });
+
+    doc.save("configurazione-hangar.pdf");
+}
